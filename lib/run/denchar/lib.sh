213ef80a153fe1be6edcb8bb434948e140c6e62b
Tom Hornos <tom.hornos@gmail.com>
2010-09-27 13:15:16 +0200

    fix siesta

---
+++ lib/run/denchar/lib.sh
@@ -0,0 +1,14 @@
+
+sp_f_load run/siesta
+
+function sp_f_denchar_prepare() {
+  sp_f_siesta_prepare "${sp_s_sion}"
+}
+
+function sp_f_denchar_finish() {
+  sp_f_siesta_finish
+}
+
+function __SP_denchar_collect() {
+  sp_f_siesta_collect
+}
