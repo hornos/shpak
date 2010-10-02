a801ad818257f6b51aae971819e8b735bff58c2e
Tom Hornos <tom.hornos@gmail.com>
2010-10-02 22:17:29 +0200

    fix run

--- lib/k.sh
+++ lib/k.sh
@@ -13,7 +13,6 @@ sp_p_queues="${sp_p_user}/queues"
 sp_p_keys="${sp_p_user}/keys"
 sp_p_lck="${sp_p_user}/lock"
 sp_p_remote="${HOME}/remote"
-sp_p_tmp="/tmp"
 
 # suffixes ----------------------------------------------------------------------
 sp_s_lib=".sh"
