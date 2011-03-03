34ec3539e8070c39795a8ac27c10d4013878d34d
Tom Hornos <tom.hornos@gmail.com>
2011-03-03 03:06:47 +0100

    google

---
+++ lib/g/lib.sh
@@ -0,0 +1,17 @@
+
+sp_g_gtu="http://ajax.googleapis.com/ajax/services/language/translate?v=1.0"
+
+sp_f_gt() {
+  # http://www.devharb.com/a-handy-bash-function-leveraging-google-api-to-do-translation-job-in-terminal/
+  local _s="${1:-hello world}"
+  local _f=${2:-en}
+  local _t=${3:-hu}
+  local _r=""
+  _r=$(wget -qO- "${sp_g_gtu}&q=${_s}&langpair=${_f}|${_t}" \
+  | sed -E -n 's/[[:alnum:]": {}]+"translatedText":"([^"]+)".*/\1/p')
+  if test "${_r}" = "" ; then
+    return 1
+  fi
+  echo ${_r}
+  return 0
+}
--- lib/gui/lib.sh
+++ lib/gui/lib.sh
@@ -1,8 +1,12 @@
-
+#f3--&7-9-V13------21-------------------42--------------------64------72
+# GLOBALS
 sp_g_cdb='{none:0,white:37,yellow:33,purple:35,red:31,cyan:36,green:32,blue:34,black:30'
 sp_g_fst='{normal:0,bold:1,under:4,blink:5,inv:7,conc:8}'
-sp_g_ces='\033['
+sp_g_esc='\033'
 
+
+#f3--&7-9-V13------21-------------------42--------------------64------72
+# COLOR
 function sp_f_ctxt() {
   local _txt=${1:-color}
   local _fg=${2:-none}
@@ -19,16 +23,50 @@ function sp_f_ctxt() {
 
   # fg
   if test "${_fg}" != "none" ; then
-    _head="${sp_g_ces}${_fst};${_cfg}m"
-    _tail="${sp_g_ces}0m"
+    _head="${sp_g_esc}[${_fst};${_cfg}m"
+    _tail="${sp_g_esc}[0m"
   fi
   # bg
   if test "${_bg}" != "none" ; then
     _cbg=$((_cbg+10))
-    _head="${_head}${sp_g_ces}${_cbg}m"
+    _head="${_head}${sp_g_esc}[${_cbg}m"
     if test "${_tail}" == "" ; then
-    _tail="${sp_g_ces}0m"
+    _tail="${sp_g_esc}[0m"
     fi
   fi
   echo -e "${_head}${_txt}${_tail}"
 }
+
+
+#f3--&7-9-V13------21-------------------42--------------------64------72
+# CHARACTER
+function sp_f_btxt() {
+  local _oe=${1:-140}
+  local _o=${2:-true}
+  if ${_o} ; then
+    _oe="\\0${_oe}"
+  fi
+  echo -e "${sp_g_esc}(0${_oe}${sp_g_esc}(B"
+}
+
+function sp_f__c() {
+  local _s=${1:-case}
+  local _d=${2:-false}
+  if ${_d} ; then
+    echo "${_s}" | tr '[:upper:]' '[:lower:]'
+  else
+    echo "${_s}" | tr '[:lower:]' '[:upper:]'
+  fi
+}
+
+function sp_f_lc() {
+  sp_f__c "${1}" true
+}
+
+function sp_f_uc() {
+  sp_f__c "${1}"
+}
+
+
+#f3--&7-9-V13------21-------------------42--------------------64------72
+# DRAW
