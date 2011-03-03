311d6e8089bd57ceebc92aca5216eb841388b5b2
Tom Hornos <tom.hornos@gmail.com>
2011-03-03 18:33:42 +0100

    fix

--- lib/i/go/lib.sh
+++ lib/i/go/lib.sh
@@ -3,9 +3,9 @@ sp_g_gtu="http://ajax.googleapis.com/ajax/services/language/translate?v=1.0"
 
 sp_f_gt() {
   # http://www.devharb.com/a-handy-bash-function-leveraging-google-api-to-do-translation-job-in-terminal/
-  local _s="${1:-hello world}"
-  local _f=${2:-en}
-  local _t=${3:-hu}
+  local _s="${1:-sok alma}"
+  local _f=${2:-hu}
+  local _t=${3:-tr}
   local _r=""
   _r=$(wget -qO- "${sp_g_gtu}&q=\"${_s}\"&langpair=${_f}|${_t}" \
   | sed -E -n 's/[[:alnum:]": {}]+"translatedText":"([^"]+)".*/\1/p' \
