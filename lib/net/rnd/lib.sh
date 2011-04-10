0870289c2959387793d41b8099bee25c93aa35da
Tom Hornos <tom.hornos@gmail.com>
2011-04-10 20:31:33 +0200

    longer random strings

--- lib/net/rnd/lib.sh
+++ lib/net/rnd/lib.sh
@@ -1,7 +1,8 @@
 #f3--&7-9-V13------21-------------------42--------------------64------72
 
-sp_g_rndu="http://www.random.org"
+sp_g_rnd_url="http://www.random.org"
 sp_g_rnd_opts="&format=plain&rnd=new"
+sp_g_rnd_msl=21
 
 function sp_f_rint() {
   local _num=${1:-1}
@@ -10,7 +11,7 @@ function sp_f_rint() {
   local _col=${4:-1}
   local _bas=${5:-10}
   local _x=${6:-7}
-  local _url="${sp_g_rndu}/integers"
+  local _url="${sp_g_rnd_url}/integers"
   _url="${_url}/?num=${_num}&min=${_min}&max=${_max}&col=${_col}&base=${_bas}${sp_g_rnd_opts}"
   wget -T ${_x} -qO- "${_url}"
 }
@@ -20,9 +21,25 @@ function sp_f_rseq() {
   local _max=${2:-10}
   local _col=${3:-1}
   local _x=${4:-7}
-  local _url="${sp_g_rndu}/sequences"
+  local _url="${sp_g_rnd_url}/sequences"
   _url="${_url}/?min=${_min}&max=${_max}&col=${_col}${sp_g_rnd_opts}"
   wget -T ${_x} -qO- "${_url}"
+}
+
+function sp_f__rstr() {
+  local _len=${1:-13}
+  local _num=${2:-1}
+  local _dig=${3:-on}
+  local _ual=${4:-on}
+  local _lal=${5:-on}
+  local _uni=${6:-on}
+  local _x=${7:-7}
+  local _url="${sp_g_rnd_url}/strings"
+  if test ${_len} -lt 21 ; then
+    _url="${_url}/?num=${_num}&len=${_len}&digits=${_dig}&upperalpha=${_ual}&loweralpha=${_lal}&unique=${_uni}${sp_g_rnd_opts}"
+    wget -T ${_x} -qO- "${_url}"
+    return $?
+  fi
 }
 
 function sp_f_rstr() {
@@ -33,9 +50,28 @@ function sp_f_rstr() {
   local _lal=${5:-on}
   local _uni=${6:-on}
   local _x=${7:-7}
-  local _url="${sp_g_rndu}/strings"
-  _url="${_url}/?num=${_num}&len=${_len}&digits=${_dig}&upperalpha=${_ual}&loweralpha=${_lal}&unique=${_uni}${sp_g_rnd_opts}"
-  wget -T ${_x} -qO- "${_url}"
+
+  if test ${_len} -lt ${sp_g_rnd_msl} ; then
+    sp_f__rstr ${_len} ${_num} ${_dig} ${_ual} ${_lal} ${_uni} ${_x}
+    return $?
+  fi
+  # string from a multi-query
+  local _il=$((${_len}/${sp_g_rnd_msl}))
+  local _rl=$((${_len}%${sp_g_rnd_msl}))
+  echo "Required random.org queries: $((${_il}+1))"
+  sp_f_yesno "Continue?"
+  if test $? -gt 0 ; then
+    echo "2"
+    return ${_FALSE_}
+  fi
+  local _rstr=""
+  # full
+  for((c=0;c<_il;++c)) ; do
+    _rstr="${_rstr}$(sp_f__rstr 20 ${_num} ${_dig} ${_ual} ${_lal} ${_uni} ${_x} 2> /dev/null)"
+  done
+  # residue
+  _rstr="${_rstr}$(sp_f__rstr ${_rl} ${_num} ${_dig} ${_ual} ${_lal} ${_uni} ${_x} 2> /dev/null)"
+  echo -e "\n${_rstr}\n"
 }
 
 function sp_f_rquo() {
@@ -44,7 +80,7 @@ function sp_f_rquo() {
   if test -n "${_ip}" ; then
     _ip="ip=${_ip}&"
   fi
-  local _url="${sp_g_rndu}/quota"
+  local _url="${sp_g_rnd_url}/quota"
   _url="${_url}/?${_ip}format=plain"
   wget -T ${_x} -qO- "${_url}"
 }
