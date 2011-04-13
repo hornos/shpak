1c6b6506500ec76c40723290801ff66822a4a79c
Tom Hornos <tom.hornos@gmail.com>
2011-04-13 23:28:16 +0200

    fix

--- lib/enc/gpg/lib.sh
+++ lib/enc/gpg/lib.sh
@@ -4,14 +4,11 @@ sp_f_load enc/base
 function sp_f_gpg___() {
   local _s="${1}"
   local _d=${2:-false}
-  local _es=""
 
   if ${_d} ; then
-    _es=$(sp_f_b64dec "${_s}" | ${sp_b_gpg} -d 2>/dev/null)
-    sp_f_b64dec "${_es}"
+    sp_f_b64dec "$(sp_f_b64dec "${_s}" | ${sp_b_gpg} -d 2>/dev/null)"
   else
-    _es=$(sp_f_b64enc "${_s}" | ${sp_b_gpg} --symmetric 2>/dev/null)
-    sp_f_b64enc "${_es}"
+    sp_f_b64enc "$(sp_f_b64enc "${_s}" | ${sp_b_gpg} -a --symmetric 2>/dev/null)"
   fi
 }
 
