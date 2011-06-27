10fa798c7fccc40f00720464f93fe2b4c0286373
Tom Hornos <tom.hornos@gmail.com>
2011-06-28 00:43:05 +0200

    paw init

---
+++ lib/run/paw/lib.sh
@@ -0,0 +1,96 @@
+function sp_f_paw_check() {
+  local _inp=$(sp_f_inm "${MAININPUT}")
+  local _p_if=""
+  local _lib=""
+  local _p_lib=""
+  local _s
+  local _sarr="${sp_s_pcntl} ${sp_s_pgeom}"
+  local _r
+
+  # check main inputs
+  for _s in  ${_sarr}; do
+    _p_if="${INPUTDIR}/${_inp}${_s}"
+    if ! test -r "${_p_if}" ; then
+      sp_f_err_fnf "${_p_if}"
+      return ${_FALSE_}
+    fi
+  done
+
+  # check libs
+  for _lib in ${LIBS}; do
+    _p_lib="${LIBDIR}/${_lib}"
+    if ! test -f "${_p_lib}" ; then
+      sp_f_err_fnf "${_p_lib}"
+      return ${_FALSE_}
+    fi
+  done
+
+  # check others
+  sp_f_run_check_others
+  _r=$?
+  if test ${_r} -gt 0 ; then
+    return ${_r}
+  fi
+
+  return ${_TRUE_}
+}
+
+function sp_f_paw_prepare() {
+  local _inp=$(sp_f_inm "${MAININPUT}")
+  local _p_if=""
+  local _dst=""
+  local _p_wdir=$(sp_f_inm "${WORKDIR}" "@")
+  local _p_sdir="${STAGEDIR}"
+  local _isd=false
+
+  if sp_f_ird "${WORKDIR}" "@" ; then 
+    _isd=true
+  fi
+
+  # prepare inputs
+  _inp=${_inp%%${sp_s_pcntl}}
+  _dst="${_inp}${sp_s_pcntl}"
+  _p_if="${INPUTDIR}/${_dst}"
+  sp_f_run_bcast ${_isd} "${_p_wdir}" "${_p_sdir}" "${_p_if}" "${_dst}"
+  if test $? -gt 0 ; then
+    return $?
+  fi
+
+  _inp=${_inp%%${sp_s_geom}}
+  _dst="${_inp}${sp_s_geom}"
+  _p_if="${INPUTDIR}/${_dst}"
+  sp_f_run_bcast ${_isd} "${_p_wdir}" "${_p_sdir}" "${_p_if}" "${_dst}"
+  if test $? -gt 0 ; then
+    return $?
+  fi
+
+  # prepare libs
+  if ! test -d "${LIBDIR}" ; then
+    sp_f_wrn "does not exist ${LIBDIR}"
+  fi
+  sp_f_run_prepare_libs ${_isd} "${_p_wdir}" "${_p_sdir}"
+
+  # prepare others
+  sp_f_run_prepare_others ${_isd} "${_p_wdir}" "${_p_sdir}"
+
+  return ${_TRUE_}
+}
+
+function sp_f_paw_finish() {
+  return ${_TRUE_}
+}
+
+function sp_f_paw_collect() {
+  local _inp=$(sp_f_inm "${MAININPUT}")
+  local _sfx="${1}"
+  local _p_wdir=$(sp_f_inm "${WORKDIR}")
+
+  _inp=${_inp%%${_sfx}}
+
+  sp_f_run_collect
+  if test $? -gt 0 ; then
+    return $?
+  fi
+
+  return ${_TRUE_}
+}
